a253002ccd293932e06ec94c1dd4241807a4edae
tuong <you@example.com>
2026-01-03 20:46:43 +0700

    add feature hide/ show list user

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShow: true,
+  };
+
+  handleOnClick = (event) => {
+    this.setState({
+      isShow: !this.state.isShow,
+    });
+  };
+
   render() {
     // sandwich fucking sandwich
     const { listUsers } = this.props;
@@ -8,15 +18,29 @@ class DisplayInfor extends React.Component {
 
     return (
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <div>My name's {user.name} </div>
-              <div>My age' {user.age}</div>
-              <hr />
-            </div>
-          );
-        })}
+        <div>
+          <div>
+            <span onClick={(event) => this.handleOnClick(event)}>
+              {this.state.isShow === true
+                ? "Hide list users"
+                : "Show list users"}
+            </span>
+          </div>
+        </div>
+
+        {this.state.isShow && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <hr />
+                  <div>My name's {user.name} </div>
+                  <div>My age' {user.age}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
