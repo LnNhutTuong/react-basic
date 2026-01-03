0be0f3b6b3cc82c4a9ece19710edc34096b45a9f
tuong <you@example.com>
2026-01-03 16:52:27 +0700

    add 2 event onClick, onMouseOver

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,12 +7,23 @@ class MyComponent extends React.Component {
     sex: "Mail",
   };
 
+  handleClick(event) {
+    console.log("My name is: ", this.state.name);
+  }
+
+  handleOnMouseOver(event) {
+    console.log(event.pageX);
+  }
+
   //JSX
   render() {
     return (
       <div>
         My name is {this.state.name}, I'm {this.state.age} and sex is
-        {this.state.sex};
+        {this.state.sex}
+        <br></br>
+        <button onClick={this.handleClick}>click me</button>
+        <button onMouseOver={this.handleOnMouseOver}>Move</button>
       </div>
     );
   }
